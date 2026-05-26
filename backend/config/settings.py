076914a262754dd146fc39bf28d69fa4f4f8ac22
Ritesh Singh <riteshsingh3589@gmail.com>
2026-05-26 15:37:29 +0530

    gunicorn

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # Put CorsMiddleware before CommonMiddleware
     'django.middleware.common.CommonMiddleware',
